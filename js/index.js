ff78ed9577b7cd09072877e6f4a6fd9c7df63134
escttsalo <escttsalo@gmail.com>
2021-05-04 20:32:36 -0600

    first event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+//Fun Bus Color Change (Mouseover)
+const funBus = document.querySelector(".logo-heading");
+funBus.addEventListener("mouseover", (e) => {
+    e.target.style.color = "blue";
+    e.target.style.fontSize = "8em";
+});
+
